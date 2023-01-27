d15ae6044d89013e69fc64e938490cc00cf25777
AkikoWada <ak.wada@gmail.com>
2023-01-27 20:30:00 +0900

    Lesson3 Javaプログラミング入門の課題提出

---
+++ src/Review01.java
@@ -0,0 +1,21 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        // 商品価格と税率（％）
+        int price=1500;
+        int taxrate=10;
+        // 消費税額
+        int taxamount;
+        // taxメソッドに商品価格と税率を渡して消費税額を戻り値として受け取る
+        taxamount=tax(price,taxrate);
+        // 税込み価格などを表示させる
+        System.out.println(price+"円の商品の税込価格は"+(price+taxamount)+"円（消費税は"+taxamount+"円）です。");
+    }
+
+    public static int tax(int price,int taxrate) {
+        int taxamount=price*taxrate/100;
+        return taxamount;
+    }
+
+}
